f67c1b2a875ee295e7792f8393e61a99cda5b671
RohanSangle <rohan.sangle02@gmail.com>
2023-10-22 20:17:58 +0530

    functioning web app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import { MapContainer, TileLayer } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import background from "./images/pattern-bg.png"
+import arrow from "./images/icon-arrow.svg"
+import Markerposition from "./components/Markerposition"
+
+import './App.css'
+
 
 function App() {
+  const [address, setAddress] = useState(null)
+  const [ipAddress, setIpAddress] = useState("")
+  const checkIpAddress =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi
+  const checkDomain =
+    /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/
+
+  useEffect(() => {
+    try {
+      const getInitialData = async () => {
+        const res = await fetch(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=8.8.8.8`
+        )
+        const data = await res.json()
+        setAddress(data)
+      }
+
+      getInitialData()
+    } catch (error) {
+      console.trace(error)
+    }
+  }, [])
+
+  const getEnteredData = async () => {
+    const res = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${
+        process.env.REACT_APP_API_KEY
+      }&${
+        checkIpAddress.test(ipAddress)
+          ? `ipAddress=${ipAddress}`
+          : checkDomain.test(ipAddress)
+          ? `domain=${ipAddress}`
+          : ""
+      }`
+      // https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=8.8.8.8&domain=google.com
+    )
+    const data = await res.json()
+    setAddress(data)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getEnteredData()
+    setIpAddress("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section>
+        <div className="">
+          <img src={background} alt="" className="" />
+        </div>
+
+        <div className="">
+          <h1 className="">
+            IP Address Tracker
+          </h1>
+
+          <form
+            onSubmit={handleSubmit}
+            autoComplete="off"
+            className=""
+          >
+            <input
+              type="text"
+              name="ipaddress"
+              id="ipaddress"
+              placeholder="Search for any IP address or domain"
+              className=""
+              value={ipAddress}
+              onChange={(e) => setIpAddress(e.target.value)}
+            />
+            <button type="submit" className="">
+              <img src={arrow} alt="" />
+            </button>
+          </form>
+        </div>
+
+        {address && (
+          <>
+            <article className="">
+              <div
+                className=""
+                style={{
+                  zIndex: 10000,
+                }}
+              >
+                <article className="">
+                  <h2 className="">
+                    IP Address
+                  </h2>
+                  <p className="">
+                    {address.ip}
+                  </p>
+                </article>
+
+                <article className="">
+                  <h2 className="">Location</h2>
+                  <p className="">
+                    {address.location.city}, {address.location.region}
+                  </p>
+                </article>
+
+                <article className="">
+                  <h2 className="">Timezone</h2>
+                  <p className="">
+                    UTC {address.location.timezone}
+                  </p>
+                </article>
+
+                <article className="">
+                  <h2 className="">ISP</h2>
+                  <p className="">
+                    {address.isp}
+                  </p>
+                </article>
+              </div>
+            </article>
+
+            <MapContainer
+              // center={[59,71]}
+              center={[address.location.lat, address.location.lng]}
+              zoom={13}
+              scrollWheelZoom={true}
+              style={{ height: "100vh", width: "100vw" }}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Markerposition address={address} />
+            </MapContainer>
+          </>
+        )}
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
